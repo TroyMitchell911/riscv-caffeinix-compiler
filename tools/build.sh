92d519f980ef17502e72e6bc4757455aaaa4a374
Troy Mitchell <TroyMitchell988@gmail.com>
2024-10-12 14:02:56 +0800

    add tools/build.sh to build tools

---
+++ tools/build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+root_path=$(pwd)
+
+mkdir -p build && cd build
+
+$root_path/autoconf-2.68/configure --prefix="$root_path/bin"
+
+make && make install
+
+export PATH=$PATH:$root_path/bin
+
+rm -rf *
+
+$root_path/automake-1.11/configure --prefix="$root_path/bin"
+
+make && make install
+
+cd ..
+
+rm -rf build
+
